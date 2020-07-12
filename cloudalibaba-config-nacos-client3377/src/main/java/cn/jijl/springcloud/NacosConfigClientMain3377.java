72dd7b975347a7245ddf4095b1bfd8fca4ad71c0
jijilan <1572877719@qq.com>
2020-07-12 23:12:04 +0800

    11112312

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/jijl/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package cn.jijl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author jijl
+ * @Date 21:09 2020/7/12
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
